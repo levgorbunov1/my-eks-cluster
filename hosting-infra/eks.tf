31ff2940be595281fc0402bbd5628b00cfab32ed
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-01 18:18:59 +0100

    ingress configuration, install ingress controller onto cluster and setup iam and oidc infrastructure for ingress controller

--- hosting-infra/eks.tf
+++ hosting-infra/eks.tf
@@ -13,6 +13,16 @@ resource "aws_eks_cluster" "eks_cluster" {
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
 }
 
+data "tls_certificate" "eks_cluster_certificate" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "cluster_oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_cluster_certificate.certificates[0].sha1_fingerprint]
+  url             = data.tls_certificate.eks_cluster_certificate.url
+}
+
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
