274d96907be66cd1ee808c54dda5b1c8b762b993
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-05 16:17:43 +0100

    create cname record instead of a record

--- dns-infra/route53.tf
+++ dns-infra/route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_zone" "webapp_route53_zone" {
 resource "aws_route53_record" "webapp_route53_a_record" {
   zone_id = aws_route53_zone.webapp_route53_zone.zone_id
   name    = var.domain
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [var.loadbalancer_dns_name]
 }
