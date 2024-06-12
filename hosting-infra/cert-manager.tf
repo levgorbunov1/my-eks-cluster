46492068a84b3671b0f3e8be1a880c96ef0dca11
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-12 20:45:26 +0100

    correct cert-manager assume role policy

--- hosting-infra/cert-manager.tf
+++ hosting-infra/cert-manager.tf
@@ -1,17 +1,24 @@
 resource "aws_iam_role" "cert_manager_role" {
   name = "cert-manager-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.cert_manager_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "cert_manager_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster_oidc_provider.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:cert-manager"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.cluster_oidc_provider.arn]
+      type        = "Federated"
+    }
+  }
 }
 
 resource "aws_iam_policy" "cert_manager_iam_policy" {
