9efbbe82a8726ed3b1103b05283011220514be51
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-07-04 11:59:15 +0100

    command changes

--- runners-test/karpenter/karpenter.sh
+++ runners-test/karpenter/karpenter.sh
@@ -11,17 +11,22 @@
 #     rolearn: arn:aws:iam::368155700659:role/KarpenterNodeRole-webapp-eks-cluster
 #     username: system:node:{{EC2PrivateDNSName}}
 
-# Generate Karpenter manifests
+# Create namespace
+# kubectl create namespace karpenter
+
+# Generate Karpenter manifests - one off
 # helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version v0.29.0 --namespace karpenter \
 #     --set settings.aws.defaultInstanceProfile=KarpenterNodeInstanceProfile-webapp-eks-cluster \
 #     --set settings.aws.clusterName=webapp-eks-cluster \
 #     --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="arn:aws:iam::368155700659:role/KarpenterControllerRole-webapp-eks-cluster" > karpenter.yaml
 
 # Create Karpenter CRDs
-# kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.sh_provisioners.yaml &&\
-# kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.k8s.aws_awsnodetemplates.yaml &&\
-# kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.sh_machines.yaml 
+kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.sh_provisioners.yaml &&\
+kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.k8s.aws_awsnodetemplates.yaml &&\
+kubectl create -f https://raw.githubusercontent.com/aws/karpenter/v0.29.0/pkg/apis/crds/karpenter.sh_machines.yaml 
 
-# Create namespace, deploy Karpenter and provisioner
-kubectl create namespace
-kubectl apply -f karpenter.yaml -f provisioner.yml
+# Deploy Karpenter
+kubectl apply -f karpenter.yaml 
+
+# Deploy provisioner
+kubectl apply -f provisioner.yml
--- runners-test/keda/deploy_scaled_job.sh
+++ runners-test/keda/deploy_scaled_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /Users/lev.gorbunov/analytical-platform-actions-runner
-helm upgrade my-releasa chart -f test-values.yml
+helm install my-releasa chart -f test-values.yml
