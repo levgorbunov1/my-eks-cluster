f29f1880c0e93dc5169f11958c12f5e82d9c5a0b
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-09-13 23:52:07 +0100

    CSI driver deployment

---
+++ hosting-infra/csi-driver.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "csi_driver_service_account_role" {
+  name = "csi-driver-role"
+  assume_role_policy = data.aws_iam_policy_document.csi_driver_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "csi_driver_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster_oidc_provider.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:csi-driver"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.cluster_oidc_provider.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "csi_driver_iam_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.csi_driver_service_account_role
+}
