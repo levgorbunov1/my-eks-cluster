7c8863cac15519df8e6f2293d2dd80decd049c72
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-09-16 21:40:41 +0100

    kms policy for ebs csi driver role

--- hosting-infra/csi-driver.tf
+++ hosting-infra/csi-driver.tf
@@ -21,7 +21,35 @@ data "aws_iam_policy_document" "csi_driver_assume_role_policy" {
   }
 }
 
+resource "aws_iam_policy" "ebs_csi_driver_kms_policy" {
+  name = "EBS-CSI-KMS-Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "kms:CreateGrant",
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "arn:aws:kms:eu-west-2:368155700659:key/*"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_role_policy_attachment" "csi_driver_iam_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.csi_driver_service_account_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_kms_policy_attachment" {
+  policy_arn = aws_iam_policy.ebs_csi_driver_kms_policy.arn
   role       = aws_iam_role.csi_driver_service_account_role.name
 }
